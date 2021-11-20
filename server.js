c83ea12d7bb514c81ddf5155a1e5d7ec59ca071a
Daniel Rodriguez <danielrodriguez@Daniels-MBP.home>
2021-11-20 00:09:53 -0500

    user auth model and routes

--- server.js
+++ server.js
@@ -5,11 +5,20 @@ const { PORT = 3000 } = process.env
 const cors = require("cors")
 const morgan = require("morgan")
 const mongoose = require("./db/db")
+const AuthRouter = require("./controllers/user")
+const auth = require("./auth")
 
 //middleware 
 app.use(express.json());
 app.use(cors());
 app.use(morgan("tiny"))
+
+//auth
+app.get("/", auth, (req, res) =>{
+    res.json(req.payload)
+})
+
+app.use("/auth", AuthRouter)
 
 //server listener 
 
